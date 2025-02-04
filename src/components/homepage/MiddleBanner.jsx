654ca2a9c76ab19db67145aa14626a899c29664c
AbhinavJaiju <abhinavchalakudi@gmail.com>
2025-02-04 15:14:21 +0530

    text changed to kottayam

--- src/components/homepage/MiddleBanner.jsx
+++ src/components/homepage/MiddleBanner.jsx
@@ -4,7 +4,7 @@ const MiddleBanner = () => {
   return (
     <div className='bg-white flex justify-center item-center text-center max-w-md mx-auto py-9'>
         <span className='max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-5xl xl:max-w-7xl text-2xl font-b'>
-        Kochi's Best Institute
+        Kottayam's Best Institute
         "An Investment In The Knowledge Pays The Best Interest"
         </span>
     </div>
